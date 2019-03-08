e186b1176976505b44355f21bdfe1bc816aae4f8
Doug Brandl <djbrandl@gmail.com>
2019-03-08 15:48:24 -0500

    Adding some code to other services before branching for a refactor.

--- ISBM.Web/Services/NotificationService.cs
+++ ISBM.Web/Services/NotificationService.cs
@@ -1,4 +1,6 @@
-﻿using ISBM.ServiceDefinitions;
+﻿using AutoMapper;
+using ISBM.ServiceDefinitions;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,11 +9,15 @@ using System.Xml.Serialization;
 
 namespace ISBM.Web.Services
 {
-    public class NotificationService : INotificationServiceSoap
+    public class NotificationService : ServiceBase, INotificationServiceSoap
     {
+        public NotificationService(DbContext dbContext, IMapper mapper) : base(dbContext, mapper) { }
+
         public void NotifyListener(string SessionID, string MessageID, [XmlElement("Topic")] string[] Topic, string RequestMessageID)
         {
-            throw new NotImplementedException();
+            // check topic as it cannot be used for consumer request session response notification
+
+            //RequestMessageID allows correlation with the original request and thus it MUST only be used for consumer request session response notification.
         }
     }
 }
--- ISBM.Web/Services/ProviderPublicationService.cs
+++ ISBM.Web/Services/ProviderPublicationService.cs
@@ -1,4 +1,7 @@
-﻿using ISBM.ServiceDefinitions;
+﻿using AutoMapper;
+using ISBM.Data.Models;
+using ISBM.ServiceDefinitions;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +11,37 @@ using System.Xml.Serialization;
 
 namespace ISBM.Web.Services
 {
-    public class ProviderPublicationService : IProviderPublicationServiceSoap
+    public class ProviderPublicationService : ServiceBase, IProviderPublicationServiceSoap
     {
+        public ProviderPublicationService(DbContext dbContext, IMapper mapper) : base(dbContext, mapper) { }
+
+        #region Private Methods
+        private ISBM.Data.Models.Channel GetChannelByUri(string channelUri)
+        {
+            return this.appDbContext.Set<ISBM.Data.Models.Channel>()
+                            .Include(m => m.ChannelsSecurityTokens).ThenInclude(cst => cst.SecurityToken).FirstOrDefault(m => m.URI.ToLower() == channelUri.ToLower());
+        }
+
+        private bool DoPermissionsMatchChannel(ISBM.Data.Models.Channel channel)
+        {
+            var permissionsToken = this.GetAccessToken();
+            return !(channel.ChannelsSecurityTokens.Any() && !channel.ChannelsSecurityTokens.Select(m => m.SecurityToken?.Token).Contains(permissionsToken));
+        }
+
+        private ISBM.Data.Models.Session GetSessionById(Guid sessionId)
+        {
+            return this.appDbContext.Set<ISBM.Data.Models.Session>()
+                            .Include(m => m.Channel).ThenInclude(m => m.ChannelsSecurityTokens).ThenInclude(cst => cst.SecurityToken).FirstOrDefault(m => m.Id == sessionId);
+        }
+
+        private bool DoPermissionsMatchSession(ISBM.Data.Models.Session session)
+        {
+            var permissionsToken = this.GetAccessToken();
+            return !(session.Channel.ChannelsSecurityTokens.Any() && !session.Channel.ChannelsSecurityTokens.Select(m => m.SecurityToken?.Token).Contains(permissionsToken));
+        }
+
+        #endregion
+
         public void ClosePublicationSession(string SessionID)
         {
             throw new NotImplementedException();
@@ -22,12 +54,70 @@ namespace ISBM.Web.Services
 
         public string OpenPublicationSession(string ChannelURI)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrWhiteSpace(ChannelURI))
+            {
+                throw new ChannelFaultException("ChannelURI cannot be null or empty.", new ArgumentNullException("ChannelURI"));
+            }
+            var channel = GetChannelByUri(ChannelURI);
+            if (channel == null)
+            {
+                throw new ChannelFaultException("A channel with the specified URI does not exist.");
+            }
+            if (!DoPermissionsMatchChannel(channel))
+            {
+                throw new ChannelFaultException("Provided header security token does not match the token assigned to the channel.");
+            }
+            if (channel.Type != (int)ChannelType.Publication)
+            {
+                throw new OperationFaultException("Channel type is not of type \"Publication\".");
+            }
+            var session = new Session
+            {
+                                ChannelId = channel.Id,
+                                Type = (int)SessionType.Publisher
+                            };
+            this.appDbContext.Set<Session>().Add(session);
+            this.appDbContext.SaveChanges();
+            return session.Id.ToString();
         }
 
         public string PostPublication(string SessionID, XmlElement MessageContent, [XmlElement("Topic")] string[] Topic, [XmlElement(DataType = "duration")] string Expiry)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrWhiteSpace(SessionID))
+            {
+                throw new SessionFaultException("SessionID cannot be null or empty.", new ArgumentNullException("SessionID"));
+            }
+            var session = GetSessionById(new Guid(SessionID));
+            if (session == null)
+            {
+                throw new SessionFaultException("A session with the specified ID does not exist.");
+            }
+            if (!DoPermissionsMatchSession(session))
+            {
+                throw new SessionFaultException("Provided header security token does not match the token assigned to the session's channel.");
+            }
+            var subscriberSessions = this.appDbContext.Set<Session>()
+                .Where(m => m.Type == (int)SessionType.Subscriber && m.ChannelId == session.ChannelId)
+                .Select(m => m.Id);
+            var message = new Message
+            {
+                Id = Guid.NewGuid(),
+                CreatedOn = DateTime.UtcNow,
+                CreatedBySessionId = session.Id,
+                Type = (int)MessageType.Publication,
+                MessageBody = MessageContent.OuterXml,
+                MessageTopics = Topic.Select(m =>
+                    new MessageTopic
+                    {
+                        Topic = m
+                    }).ToList(),
+                MessagesSessions = subscriberSessions.Select(m => new MessagesSession
+                {
+                    SessionId = m
+                }).ToArray()  
+            };
+
+            return string.Empty;
         }
     }
 }
