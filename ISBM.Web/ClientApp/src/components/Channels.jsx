7502aa9f6dd69eca3a364fc18aeb3772a5205f6e
Doug Brandl <djbrandl@gmail.com>
2019-03-15 18:09:26 -0400

    Getting some of the channel front-end working to demonstrate API usage.

--- ISBM.Web/ClientApp/src/components/Channels.jsx
+++ ISBM.Web/ClientApp/src/components/Channels.jsx
@@ -2,53 +2,131 @@
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { actionCreators } from '../store/Channels';
-
+import { TabContent, TabPane, Nav, NavItem, NavLink, Row, Col, Form, FormGroup, FormText, Label, Input, Button } from 'reactstrap';
+import classnames from 'classnames';
 
 class FetchData extends Component {
     componentDidMount() {
-        // This method is called when the component is first added to the document
-        this.ensureDataFetched();
-    }
-    componentDidUpdate() {
-        // This method is called when the route parameters change
         this.ensureDataFetched();
     }
     ensureDataFetched() {
         this.props.requestChannels();
     }
+
     render() {
         return (
             <div>
                 <h1>Channels</h1>
-                <hr/>
-                <p>This component demonstrates fetching channels from the REST API.</p>
-                {renderChannels(this.props)}
+                <p>This component demonstrates functionality of the Channel REST API.</p>
+                <hr />
+                <Nav tabs>
+                    <NavItem>
+                        <NavLink className={classnames({ active: this.props.activeTab === 'Get' })} onClick={() => { this.props.setActiveTab('Get'); }}>Get Channels</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className={classnames({ active: this.props.activeTab === 'Create' })} onClick={() => { this.props.setActiveTab('Create') }}>Create Channel</NavLink>
+                    </NavItem>
+                </Nav>
+                <TabContent activeTab={this.props.activeTab}>
+                    <TabPane tabId="Get">
+                        <Row>
+                            <Col sm="12">
+                                <br />
+                                {renderGetChannels(this.props)}
+                            </Col>
+                        </Row>
+                    </TabPane>
+                    <TabPane tabId="Create">
+                        <Row>
+                            <Col sm="12">
+                                <br/>
+                                {renderCreateForm(this.props)}
+                            </Col>
+                        </Row>
+                    </TabPane>
+                </TabContent>
+                {renderSelectedChannel(this.props)}
             </div>
         );
     }
 }
 
-function renderChannels(props) {
+function renderCreateForm(props) {
     return (
-        <table className='table table-striped'>
-            <thead>
-                <tr>
-                    <th>URI</th>
-                    <th>Type</th>
-                    <th>Topics</th>
-                </tr>
-            </thead>
-            <tbody>
-                {props.channels.map(channel =>
-                    <tr key={channel.ChannelUri}>
-                        <td>{channel.uri}</td>
-                        <td>{channel.type}</td>
-                        <td>{channel.tokens}</td>
-                            
-                    </tr>
-                )}
-            </tbody>
-        </table>
+        <Form onSubmit={props.createChannel}>
+            <h2>Create a new channel</h2>
+            <FormGroup row>
+                <Label for="channelUri" sm={2}>URI</Label>
+                <Col sm={10}>
+                    <Input type="text" name="uri" id="uri" placeholder="Channel URI" />
+                </Col>
+            </FormGroup>
+            <FormGroup row>
+                <Label for="channelType" sm={2}>Type</Label>
+                <Col sm={10}>
+                    <Input type="select" name="type">
+                        <option>Publication</option>
+                        <option>Request</option>
+                    </Input>
+                </Col>
+            </FormGroup>
+            <FormGroup row>
+                <Label for="description" sm={2}>Description</Label>
+                <Col sm={10}>
+                    <Input type="textarea" name="description" placeholder="Channel Description" />
+                </Col>
+            </FormGroup>
+            <FormGroup row>
+                <Label for="securityToken" sm={2}>Security Token</Label>
+                <Col sm={10}>
+                    <Input type="password" name="securityToken" id="securityToken" placeholder="Security Token" />
+                    <FormText color="muted">
+                        Current front-end implementation of this demonstration only allows for the user to enter just 1 security token when creating a channel. The underlying API driving the behavior <strong>does</strong> allow for multiple security tokens to be assigned to the channel on creation.
+                    </FormText>
+                </Col>
+            </FormGroup>
+            <Button>Submit</Button>
+        </Form>
+    );
+}
+
+function renderSelectedChannel(props) {
+
+}
+
+function renderGetChannels(props) {
+    return (
+        <Row>
+            <Col sm="12">
+                <Form onSubmit={(e) => { props.setAccessToken(e); }}>
+                    <FormGroup row>
+                        <Label for="token" sm={3}>Set Access Token</Label>
+                        <Col sm={7}>
+                            <Input type="password" name="token" id="token" placeholder="Access Token" />
+                        </Col>
+                        <Button sm="2">Set</Button>
+                    </FormGroup>
+                </Form>
+                <table className='table table-striped'>
+                    <thead>
+                        <tr>
+                            <th>URI</th>
+                            <th>Type</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {props.channels.map(channel =>
+                            <tr key={channel.uri}>
+                                <td>{channel.uri}</td>
+                                <td>{channel.type}</td>
+                                <td>{channel.description}</td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+            </Col>
+        </Row>
     );
 }
 
--- ISBM.Web/ClientApp/src/store/Channels.js
+++ ISBM.Web/ClientApp/src/store/Channels.js
@@ -1,16 +1,79 @@
 ﻿export const REQUEST_CHANNELS = 'REQUEST_CHANNELS'
 export const RECEIVE_CHANNELS = 'RECEIVE_CHANNELS'
+export const SET_ACCESS_TOKEN = 'SET_ACCESS_TOKEN'
+export const CREATE_CHANNELS_REQUEST = 'CREATE_CHANNELS_REQUEST'
+export const CREATE_CHANNELS_RECEIVE = 'CREATE_CHANNELS_RECEIVE'
+export const CHANGE_TAB = 'CHANGE_TAB'
 
-const initialState = { channels: [], isLoading: false };
-
+const initialState = { channels: [], isLoading: false, accessToken: '', activeTab: 'Get' };
+const channelApiFunctions = {
+    getChannels: async (accessToken) => {
+        const url = 'api/Channels/';
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                "Authorization": accessToken,
+                "Content-Type": "application/json"
+            }
+        });
+        const channels = await response.json();
+        return channels;
+    }
+};
 
 export const actionCreators = {
+    setAccessToken: (event) => async (dispatch, getState) => {
+        event.preventDefault();
+        const data = new FormData(event.target);
+        var object = {};
+        data.forEach(function (value, key) {
+            object[key] = value;
+        });
+        console.log(object['token']);
+        dispatch({ type: SET_ACCESS_TOKEN, accessToken: object['token'] });
+
+        dispatch({ type: REQUEST_CHANNELS });
+        const channels = await channelApiFunctions.getChannels(object['token']);
+        dispatch({ type: RECEIVE_CHANNELS, channels });
+    },
+    setActiveTab: (tab) => async (dispatch, getState) => {
+        dispatch({ type: CHANGE_TAB, tab: tab });
+    },
+    createChannel: (event) => async (dispatch, getState) => {
+        dispatch({ type: CREATE_CHANNELS_REQUEST });
+
+        event.preventDefault();
+        const data = new FormData(event.target);
+        var object = {};
+        data.forEach(function (value, key) {
+            if (key === 'securityToken') {
+                object['securityTokens'] = [];
+                if (value !== '') {
+                    object['securityTokens'].push({ "token": value });
+                }
+            } else {
+                object[key] = value;
+            }
+        });
+
+        const accessToken = getState().channels.accessToken;
+        const url = 'api/Channels/';
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                "Authorization": accessToken,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(object)
+        });
+        await response;
+        dispatch({ type: CREATE_CHANNELS_RECEIVE });
+    },
     requestChannels: () => async (dispatch, getState) => {
         dispatch({ type: REQUEST_CHANNELS });
 
-        const url = 'api/Channels/';
-        const response = await fetch(url);
-        const channels = await response.json();
+        const accessToken = getState().channels.accessToken;
+        const channels = await channelApiFunctions.getChannels(accessToken);
 
         dispatch({ type: RECEIVE_CHANNELS, channels });
     }
@@ -21,6 +84,13 @@ export const actionCreators = {
 // do not modify the original state, but make sure you copy the state
 export const reducer = (state, action) => {
     state = state || initialState;
+
+    if (action.type === SET_ACCESS_TOKEN) {
+        return {
+            ...state,
+            accessToken: action.accessToken
+        };
+    }
 
     if (action.type === REQUEST_CHANNELS) {
         return {
@@ -37,5 +107,25 @@ export const reducer = (state, action) => {
         };
     }
 
+    if (action.type === CREATE_CHANNELS_REQUEST) {
+        return {
+            ...state,
+            isLoading: true
+        };
+    }
+
+    if (action.type === CREATE_CHANNELS_RECEIVE) {
+        return {
+            ...state,
+            isLoading: false
+        };
+    }
+
+    if (action.type === CHANGE_TAB) {
+        return {
+            ...state,
+            activeTab: action.tab
+        };
+    }
     return state;
 };
