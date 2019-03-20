cc31558a5f5420e74ff7f3862332dba857dc5269
Doug Brandl <djbrandl@gmail.com>
2019-03-20 12:43:22 -0400

    Removing old ISBM files

--- ISBM.Data/Models/BaseEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace ISBM.Data.Models
-{
-    public class BaseEntity
-    {
-        public Guid Id { get; set; }
-    }
-}
--- ISBM.Web/Models/SecurityToken.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Xml;
-
-namespace ISBM.Web.Models
-{
-    public class SecurityToken
-    {
-        public string Token { get; set; }
-    }
-}
